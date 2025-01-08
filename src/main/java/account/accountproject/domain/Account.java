e89c6bdead3a1429d362314a73f734ceea5ba0bd
HUIJAEKO <xrhgmlwox@naver.com>
2025-01-08 20:11:14 +0900

    feat: generate account table

---
+++ src/main/java/account/accountproject/domain/Account.java
@@ -0,0 +1,23 @@
+package account.accountproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+}
---
+++ src/main/java/account/accountproject/domain/AccountStatus.java
@@ -0,0 +1,6 @@
+package account.accountproject.domain;
+
+public enum AccountStatus {
+    IN_USE,
+    UNREGISTERED
+}
